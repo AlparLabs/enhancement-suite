952ae923ce712f74294db904fd5db33567accd48
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-01-29 17:34:46 -0300

    Bump manifest version

--- material_requests/__manifest__.py
+++ material_requests/__manifest__.py
@@ -1,10 +1,10 @@
 {
     'name': 'Material Requests (PIM/SIM)',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'summary': 'Manage PIM (Internal Material Requests) and SIM (Purchase Requests)',
     'author': 'AlparData',
     'category': 'Construction/Inventory',
-    'depends': ['base', 'project', 'stock', 'purchase'],
+    'depends': ['project', 'stock', 'purchase'],
     'data': [
         'security/ir.model.access.csv',
         'views/pim_views.xml',
