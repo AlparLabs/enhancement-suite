8761dd4edde76fe934f999185bd97faae7a7835d
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-04-24 11:47:35 -0300

    feat: add module to hide initial balances in general and partner ledger reports

---
+++ account_hide_initial_balances/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Account Hide Initial Balances',
+    'version': '18.0.0.0.1',
+    'category': 'Accounting/Accounting',
+    'summary': 'Hides Initial Balances in Accounting Reports',
+    'description': """
+        This module hides the initial balance in the Partner Ledger and General Ledger reports.
+    """,
+    'author': 'AlparData',
+    'website': 'https://www.alpardata.com.ar',
+    'depends': ['account_reports'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'OEEL-1',
+}
