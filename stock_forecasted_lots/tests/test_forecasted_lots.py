1aa15c56984327d33d1cb30d245228f071cf540a
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-07-08 14:56:24 -0300

    test(stock_forecasted_lots): verify lots are scoped to the report's warehouse

--- stock_forecasted_lots/tests/test_forecasted_lots.py
+++ stock_forecasted_lots/tests/test_forecasted_lots.py
@@ -40,3 +40,19 @@ class TestForecastedLots(TransactionCase):
         self.assertEqual(data['lots'][0]['available_quantity'], 30.0)
         self.assertEqual(data['lots'][1]['id'], self.lot_a.id)
         self.assertEqual(data['lots'][1]['available_quantity'], 100.0)
+
+    def test_lots_filtered_by_warehouse(self):
+        other_warehouse = self.env['stock.warehouse'].create({
+            'name': 'Depot Test',
+            'code': 'DEPT',
+        })
+        self.env['stock.quant']._update_available_quantity(
+            self.product, self.stock_location, 100.0, lot_id=self.lot_a)
+        self.env['stock.quant']._update_available_quantity(
+            self.product, other_warehouse.lot_stock_id, 50.0, lot_id=self.lot_b)
+
+        data = self.report_model.with_context(
+            warehouse_id=self.warehouse.id
+        )._get_report_data(product_ids=self.product.ids)
+
+        self.assertEqual([lot['id'] for lot in data['lots']], [self.lot_a.id])
