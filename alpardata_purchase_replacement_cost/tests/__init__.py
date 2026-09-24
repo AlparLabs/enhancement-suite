431a9128702e7802959bbc7bcca921b72afd8e6f
Santiago Tojo <santiago.tojo@alpardata.com.ar>
2026-09-24 02:47:27 -0300

    test(purchase_replacement_cost): permisos de edicion de condiciones comerciales

--- alpardata_purchase_replacement_cost/tests/__init__.py
+++ alpardata_purchase_replacement_cost/tests/__init__.py
@@ -4,4 +4,4 @@ from . import test_replacement_cost
 from . import test_pricelist
 from . import test_purchase_order
 from . import test_divergence
-# from . import test_access
+from . import test_access
